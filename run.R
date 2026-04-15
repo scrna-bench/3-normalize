1c54ae87dc2b53131dd0221dc5ad00e10280dc12
Mark Robinson <mark.robinson@mls.uzh.ch>
2026-04-15 16:15:04 +0200

    first possible running

--- run.R
+++ run.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 library(argparse)
+library(Matrix)
 
 # Parse command line arguments
 parser <- ArgumentParser(description="OmniBenchmark module")
@@ -16,26 +17,32 @@ parser$add_argument("--name", dest="name", type="character", required=TRUE,
 
 parser$add_argument("--normalization_type", dest="normalization_type", type="character", help="Input file")
 
+# parameter for 'input_h5' (comes from 1st stage)
+parser$add_argument("--rawdata.h5ad", dest="input_h5", type="character", help="input file")
+
 args <- parser$parse_args()
 
 cat("Full command: ", paste0(commandArgs(), collapse = " "))
-cat("Output directory:", args$output_dir, "\n")
-cat("Module name:", args$name, "\n")
-cat("Normalization type:", args$name, "\n")
+cat("output_dir:", args$output_dir, "\n")
+cat("name:", args$name, "\n")
+cat("normalization_type:", args$normalization_type, "\n")
+cat("input_h5:", args$input_h5, "\n")
 
-
-if (args$filter_type == "manual") {
-  qc <- metadata(sce)$qc_thresholds
-  mt_percent <- rna.qc.metrics$subsets$mt * 100
-  keep <- rna.qc.metrics$detected >= qc[qc$metric == "nFeature", "min"] &
-    rna.qc.metrics$detected <= qc[qc$metric == "nFeature", "max"] &
-    mt_percent < qc[qc$metric == "percent.mt", "max"] &
-    rna.qc.metrics$sum <= qc[qc$metric == "nCount", "max"]
-} else if (args$filter_type == "scrapper-auto") {
-  require(DelayedArray)
-  rna.qc.thresholds <- suggestRnaQcThresholds(rna.qc.metrics)
-  keep <- filterRnaQcMetrics(rna.qc.thresholds, rna.qc.metrics)
+if (args$normalization_type == "seurat_log1pCP10k") {
+  require(Seurat)
+  so <- read_h5ad(args$input_h5, as = "Seurat")
+  message("Running log1pCP10k")
+  so <- NormalizeData(so, scale.factor = 10^4)
+  d <- so[["RNA"]]$data
+} else if (args$normalization_type == "scuttle_geometricSizeFactors") {
+  require(SingleCellExperiment)
+  require(scuttle)
+  sce <- read_h5ad(args$input_h5, as = "SingleCellExperiment")
+  sce <- logNormCounts(sce, geometricSizeFactors(sce))
+  d <- Matrix(logcounts(sce), sparse = TRUE)
+} else {
+  errorCondition("incorrect 'normalization_type' specified")
 }
 
-output_file <- file.path(args$output_dir, paste0(args$name, "_cellids.txt.gz"))
-writeLines(colnames(sce)[keep], gzfile(output_file))
+output_file <- file.path(args$output_dir, paste0(args$name, "_normalized.mtx"))
+writeMM(d, output_file)
